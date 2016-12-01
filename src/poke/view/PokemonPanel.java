f725d2cbd1cd428f4a79deaf0a86f6eb8415a3fa
Arick Smith <kidwriter2020@gmail.com>
2016-12-01 13:49:18 -0700

    Made the imports and started constructing things.

--- src/poke/view/PokemonPanel.java
+++ src/poke/view/PokemonPanel.java
@@ -1,6 +1,30 @@
 package poke.view;
 
-public class PokemonPanel 
-{
+import java.awt.Color;
+import javax.swing.*;
+import poke.controller.PokemonController;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class PokemonPanel extends JPanel
+{
+	private PokemonController baseController;
+	private ImageIcon pokemonIcon;
+	private SpringLayout baseLayout;
+	private JButton updateButton;
+	private JComboBox pokedexSelector;
+	private JLabel pokemonLabel;
+	private JLabel healthLabel;
+	private JLabel combatLabel;
+	private JLabel speedLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel advancedLabelLabel;
+	private JTextField healthField;
+	private JTextField combatField;
+	private JTextField speedField;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextArea advancedArea;
 }
