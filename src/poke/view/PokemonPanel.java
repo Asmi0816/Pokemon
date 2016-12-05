288c6a7ab4df4ee0c448aa9e74151a84798b0675
Arick Smith <kidwriter2020@gmail.com>
2016-12-05 13:26:34 -0700

    Finished up the installing of every thing to the panel.

--- src/poke/view/PokemonPanel.java
+++ src/poke/view/PokemonPanel.java
@@ -20,11 +20,80 @@ public class PokemonPanel extends JPanel
 	private JLabel speedLabel;
 	private JLabel nameLabel;
 	private JLabel numberLabel;
-	private JLabel advancedLabelLabel;
+	private JLabel advancedLabel;
 	private JTextField healthField;
 	private JTextField combatField;
 	private JTextField speedField;
 	private JTextField nameField;
 	private JTextField numberField;
 	private JTextArea advancedArea;
+	
+	public PokemonPanel(PokemonController baseController)
+	{
+		this.baseController = baseController;
+		this.baseLayout = new SpringLayout();
+		this.pokemonIcon = new ImageIcon(getClass().getResource(""));
+		this.updateButton = new JButton("Update Your Pokemon!");
+		this.nameField = new JTextField(25);
+		this.combatField = new JTextField(5);
+		this.healthField = new JTextField(5);
+		this.speedField = new JTextField(5);
+		this.numberField = new JTextField(5);
+		this.advancedArea = new JTextArea (10, 25);
+		this.pokedexSelector = new JComboBox(baseController.buildPokedexText());
+		this.advancedLabel = new JLabel ("Advanced information");
+		this.combatLabel = new JLabel ("Combat Points : ");
+		this.healthLabel = new JLabel ("Health Points : ");
+		this.speedLabel = new JLabel ("Speed Rate : ");
+		this.numberLabel = new JLabel ("Pokemon Number : ");
+		this.nameLabel = new JLabel ("My Name Is :");
+		this.pokemonLabel = new JLabel ("The Current pokemon", pokemonIcon, JLabel.CENTER);
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setPreferredSize(new Dimension(900, 600));
+		this.setBackground(Color.DARK_GRAY);
+		
+		numberField.setEditable(false);
+		advancedArea.setEditable(false);
+		advancedArea.setWrapStyleWord(true);
+		advancedArea.setLineWrap(true);
+
+		pokemonLabel.setVerticalTextPosition(JLabel.BOTTOM);
+		pokemonLabel.setHorizontalTextPosition(JLabel.CENTER);
+		
+		this.add(pokedexSelector);
+		this.add(healthField);
+		this.add(healthLabel);
+		this.add(combatField);
+		this.add(combatLabel);
+		this.add(speedField);
+		this.add(speedLabel);
+		this.add(nameField);
+		this.add(nameLabel);
+		this.add(numberField);
+		this.add(numberLabel);
+		this.add(advancedArea);
+		this.add(advancedLabel);
+		this.add(pokemonLabel);
+		this.add(updateButton);
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
