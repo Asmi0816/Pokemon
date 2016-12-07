9969c92afbcc76e68d6c8f2af6992ef5a9cee20e
Asmi0816 <kidwriter2020@gmail.com>
2016-12-07 13:17:20 -0700

    Added to my controller with strings and methods and lists.

--- src/poke/controller/PokemonController.java
+++ src/poke/controller/PokemonController.java
@@ -9,8 +9,39 @@ import javax.swing.JOptionPane;
 public class PokemonController 
 {
 	private ArrayList<Pokemon> pokedex;
+	private PokemonFrame baseFrame;
+	
+	public PokemonController()
+	{
+		pokedex = new ArrayList<Pokemon>();
+		buildPokedex();
+		
+		baseFrame = new PokemonFrame(this);
+	}
+	
 	public void start()
 	{
+		JOptionPane.showMessageDialog(baseFrame, "Welcome to Pokemon Inheritence!");
+	}
+	
+	private void buildPokedex()
+	{
+		pokedex.add(new Magicarp("OP"));
+		pokedex.add(new Rhyhorn("Master"));
+		pokedex.add(new Bulbasuar("Whip"));
+		pokedex.add(new Charizard("Blaze"));
 		
 	}
+	
+	public String[] buildPokedexText()
+	{
+		String [] pokemonNames =new String[pokedex.size()];
+		for(int index = 0; index < pokedex.size(); index++)
+		{
+			pokemonNames[index] = pokedex.get(index).getName();
+		}
+		
+		return pokemonNames;
+	}
+	
 }
